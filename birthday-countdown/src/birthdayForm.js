af508c92f7688bc0b48401d6321210b6dbd76827
ayadij <ayadijkwel@yahoo.com>
2018-06-22 13:09:00 -0600

    boilerplate and birthday form component

---
+++ birthday-countdown/src/birthdayForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+
+class BirthdayForm extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+                BirthdayForm Component
+            </div>
+        )
+
+    }
+}
+
+
+export default BirthdayForm;
