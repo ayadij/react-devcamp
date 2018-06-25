2fd2fb376a32208bffc98a28ae448cd63f0269cb
ayadij <ayadijkwel@yahoo.com>
2018-06-25 16:16:17 -0600

    fixed coundown bug but other bugs remain

--- birthday-countdown/src/App.js
+++ birthday-countdown/src/App.js
@@ -6,7 +6,13 @@ class App extends Component {
   render() {
     return (
       <div> 
-        <BirthdayForm />
+        <header>
+          <div className="header_skew"> 
+            <div className="header_subskew">
+              <BirthdayForm />
+            </div>
+          </div>
+        </header>
       </div>
     );
   }
--- birthday-countdown/src/clock.js
+++ birthday-countdown/src/clock.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
+import App from './App'
 
 class Clock extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            timeRemaining: this.getTimeRemaining(props.birthdayFormState.startDate.toString())
+            timeRemaining: this.gettimeRemaining(props.birthdayFormState.startDate.toString())
         }
 
         this.timer = 0
         this.birthday = props.birthdayFormState.startDate.toString();
-        this.getTimeRemaining = this.getTimeRemaining.bind(this);
-        this.noBirthYear = new Date(this.birthday).getFullYear() != new Date().getFullYear()                                                                            
+        this.gettimeRemaining = this.gettimeRemaining.bind(this);
+        this.getAge = this.getAge.bind(this);
+        this.noBirthYear = new Date(this.birthday).getFullYear() != new Date().getFullYear()
     }
 
-    getTimeRemaining(birthday) {
+
+    gettimeRemaining(birthday) {
         var bday = new Date(birthday)
-        let today = new Date();
+        var today = new Date();
 
         const currentMonth = today.getMonth();
         const birthMonth = bday.getMonth();
+
+        bday.setFullYear(today.getFullYear());
+
+        var distance = bday.getTime() - today.getTime();
 
         if(birthMonth > currentMonth) {
             bday.setFullYear(today.getFullYear());
@@ -32,14 +39,9 @@ class Clock extends Component {
                 bday.setFullYear(today.getFullYear());
             } else if(birthDay < currentDay) {
                 bday.setFullYear(today.getFullYear() + 1);
-            } else if (birthday == currentDay){
-                return 0
             }
         }
 
-        bday.setFullYear(today.getFullYear());
-
-        var distance = bday.getTime() - today.getTime();
 
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -52,8 +54,9 @@ class Clock extends Component {
             'minutes': minutes,
             'seconds': seconds
         }
-
     }
+
+
     getAge = function() {
         var bday = new Date(this.birthday);
         let today = new Date();
@@ -61,21 +64,24 @@ class Clock extends Component {
         var distance = today.getTime() - bday.getTime();
         var daysOld = Math.floor(distance / (1000 * 60 * 60 * 24));
         var yearsOld = Number((daysOld/365).toFixed(0));
+        var noBirthYear = new Date(this.birthday).getFullYear() != new Date().getFullYear()
 
         return yearsOld
-    }.bind(this)
+    }
 
 
     componentDidMount() {
         this.timer = setInterval(() => {
-            const timeRemaining = this.getTimeRemaining(this.birthday)
+            const timeRemaining = this.gettimeRemaining(this.birthday)
             this.setState({ timeRemaining: timeRemaining })
         }, 1000);
     }
 
+
     componentWillUnmount() {
         clearInterval(this.timer);
     }
+
 
     renderMessage = function() {
         if(this.noBirthYear) {
@@ -84,37 +90,34 @@ class Clock extends Component {
         return <h4>remaining until you are {this.getAge()}</h4>
     }.bind(this)
 
+
     render(){
         const data = this.state.timeRemaining
         return (
             <div>
             {
                 this.state.timeRemaining == 0 ?
-                <h1>hbd</h1>
-                :
-                <div>
-                    <div>  
-                        <div> DAYS {data.days} </div>
-                        <div> HRS {data.hours} </div>
-                        <div> MINS {data.minutes} </div>
-                        <div> SECS {data.seconds} </div>
-                    </div>
+                    <h1>HBD</h1>
+                    :
                     <div>
-                        {this.renderMessage}
+                        <div>
+                            <div> DAYS {data.days} </div>
+                            <div> HRS {data.hours} </div>
+                            <div> MINS {data.minutes} </div>
+                            <div> SECS {data.seconds} </div>
+                        </div>
+                        <div>
+                            {<h4>remaining until you are {this.getAge()}</h4>}
+                        </div>
                     </div>
-                </div>
-            }
+            } 
             </div>
-
-
-
-
-            
-        );
-    } 
-
+        )
+    }
 }
 
 export default Clock;
 
 //https://rails.devcamp.com/comprehensive-react-development-tutorial/react-project-two-build-birthday-countdown-application/1634
+
+//https://github.com/bottega-code-school/BirthdayCountdownApp?files=1
